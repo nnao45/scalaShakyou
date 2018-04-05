be63b29a55c425042de9e85b43b1aeece667f0ce
nnao45 <n4sekai5y@gmail.com>
2018-04-05 15:02:40 +0900

    Thu Apr  5 15:02:40 JST 2018

---
+++ caseClass07/caseClass07.scala
@@ -0,0 +1,19 @@
+case class Human(name: String)
+
+object Main extends App{
+  val human1 = Human("Alice")
+  val human2 = Human("Android No.3")
+  val human3 = Human("Alice")
+
+  if(human1.equals(human2)) {
+    println(human1 + "= " + human2)
+  } else {
+    println(human1 + "≠" + human2)
+  }
+
+  if(human1.equals(human3)) {
+    println(human1 + "= " + human3)
+  } else {
+    println(human1 + "≠" + human3)
+  }
+}
