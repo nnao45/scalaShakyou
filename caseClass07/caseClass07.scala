28b88a913ede703f66851363743849e7051adb09
nnao45 <n4sekai5y@gmail.com>
2018-04-15 12:48:37 +0900

    Sun Apr 15 12:48:37 JST 2018

--- caseClass07/caseClass07.scala
+++ caseClass07/caseClass07.scala
@@ -4,7 +4,7 @@ object Main extends App{
   val human1 = Human("Alice")
   val human2 = Human("Android No.3")
   val human3 = Human("Alice")
-
+  
   if(human1.equals(human2)) {
     println(human1 + "= " + human2)
   } else {
